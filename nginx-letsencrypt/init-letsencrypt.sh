2b64b2da133c5d18a0e4564e04a1acb7857bbac1
Prashant Solanki <solankisrp2@gmail.com>
2021-03-28 17:19:25 +1100

    Updated nginx for prashantsolanki.com

--- nginx-letsencrypt/init-letsencrypt.sh
+++ nginx-letsencrypt/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(california.home www.california.home)
+domains=(***REMOVED*** www.***REMOVED***)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
