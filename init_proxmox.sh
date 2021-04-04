c0d78c1b7d8fde672af8f8121a572c8491459915
Prashant Solanki <solankisrp2@gmail.com>
2021-04-04 23:50:55 +1000

    Updated Gluster Init Proxmox single line

--- init_proxmox.sh
+++ init_proxmox.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-cd /etc/apt/sources.list.d
-mv pve-enterprise.list pve-enterprise.list.disabled
-echo 'deb http://download.proxmox.com/debian/pve buster pve-no-subscription' > pve-community.list
-apt -y update
-apt -y dist-upgrade
-apt -y install ifupdown2
-reboot
+cd /etc/apt/sources.list.d && mv pve-enterprise.list pve-enterprise.list.disabled && echo 'deb http://download.proxmox.com/debian/pve buster pve-no-subscription' > pve-community.list && apt -y update && apt -y dist-upgrade && apt -y install ifupdown2 git curl && git clone https://github.com/prashantsolanki3/docker-california ~/docker-california && cd ~/docker-california
+
--- setup_glusterfs.sh
+++ setup_glusterfs.sh
@@ -1,70 +1,70 @@
 #!/bin/bash
 
-echo "Requires host-names to be set in /etc/hosts"
+echo "Requires host-names to be set in /etc/h"
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -d /dev/sdb -dv buster -br /data/brick01 -gv gv01 -arch "
-   echo -e "\t-device Device e.g. /dev/sdb"
-   echo -e "\t-debian_version Debian Version: buster"
-   echo -e "\t-brick_path Brick Path: /data/brick01"
-   echo -e "\t-gluster_volume Gluster Volume Name: gv01"
-   echo -e "\t-arch Architecture: amd64"
-   echo -e "\t-hosts List of hostnames: pve01|pve02|pve03"
+   echo "Usage: $0 -d /dev/sdb -v buster -b /data/brick01 -g gv01 -a amd64 -h pve01|pve02"
+   echo -e "\t-d d e.g. /dev/sdb"
+   echo -e "\t-v Debian Version: buster"
+   echo -e "\t-b Brick Path: /data/brick01"
+   echo -e "\t-g Gluster Volume Name: gv01"
+   echo -e "\t-a aitecture: amd64"
+   echo -e "\t-h List of hostnames: pve01|pve02|pve03"
    exit 1 # Exit script after printing help
 }
 
-while getopts "device:debian_version:brick_path:gluster_volume:arch:hosts:" opt
+while getopts "d:v:b:g:a:h:" opt
 do
    case "$opt" in
-      device ) device="$OPTARG" ;;
-      debian_version ) debian_version="$OPTARG" ;;
-      brick_path ) brick_path="$OPTARG" ;;
-      gluster_volume ) gluster_volume="$OPTARG" ;;
-      arch ) arch="$OPTARG" ;;
-      hosts ) hosts="$OPTARG" ;;
+      d ) d="$OPTARG" ;;
+      v ) v="$OPTARG" ;;
+      b ) b="$OPTARG" ;;
+      g ) g="$OPTARG" ;;
+      a ) a="$OPTARG" ;;
+      h ) h="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
 
 # Print helpFunction in case parameters are empty
-if [ -z "$device" ] || [ -z "$debian_version" ] || [ -z "$brick_path" ] || [ -z "$gluster_volume" ] || [ -z "$arch" ] || [ -z "$hosts" ]
+if [ -z "$d" ] || [ -z "$v" ] || [ -z "$b" ] || [ -z "$g" ] || [ -z "$a" ] || [ -z "$h" ]
 then
    echo "Some or all of the parameters are empty";
    helpFunction
 fi
 
 # Begin script in case all parameters are correct
-echo "$device $debian_version $brick_path $gluster_volume $arch $hosts"
+echo "$d $v $b $g $a $h"
 
-# device=/dev/sdb
-# debian_version=buster
+# d=/dev/sdb
+# v=buster
 
 # wget -O - https://download.gluster.org/pub/gluster/glusterfs/7/rsa.pub | apt-key add -
-# echo deb [arch=$arch] https://download.gluster.org/pub/gluster/glusterfs/7/LATEST/Debian/$debian_version/$arch/apt $debian_version main > /etc/apt/sources.list.d/gluster.list 
+# echo deb [a=$a] https://download.gluster.org/pub/gluster/glusterfs/7/LATEST/Debian/$v/$a/apt $v main > /etc/apt/sources.list.d/gluster.list 
 # apt-get update -y
 # apt full-upgrade -y
 # apt install -y glusterfs-server
 # systemctl enable glusterd
 # systemctl start glusterd
 
-# mkfs.xfs -i size=512 $device
-# mkdir -p $brick_path
-# echo "$device $brick_path xfs defaults 1 2" >> /etc/fstab
+# mkfs.xfs -i size=512 $d
+# mkdir -p $b
+# echo "$d $b xfs defaults 1 2" >> /etc/fstab
 # mount -a && mount
-# mkdir -p $brick_path/$gluster_volume
+# mkdir -p $b/$g
 
 # # required probing depends on node
 # # IN="bla@some.com;john@home.com"
 replicaString = ""
-arrHosts=(${hosts//|/ })
+arrh=(${h//|/ })
 for ((n=0;n<${#distro[@]};n++))
 do
-    # gluster peer probe ${arrHosts[n]}
-    replicaString = "${arrHosts[n]}:$brick_path/$gluster_volume "
+    # gluster peer probe ${arrh[n]}
+    replicaString = "${arrh[n]}:$b/$g "
 done
 
-echo "$arrHosts $replicaString"
+echo "$arrh $replicaString"
 
-# gluster volume create $gluster_volume replica ${arrHosts[n]} $replicaString
-# gluster volume start $gluster_volume
+# gluster volume create $g replica ${arrh[n]} $replicaString
+# gluster volume start $g
